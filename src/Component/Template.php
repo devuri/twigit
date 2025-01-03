da630bea3156f22617965f2ff44d02a5d1165a2f
devuri <devuri@users.noreply.github.com>
2025-01-03 00:02:16 -0500

    fix: better message for `Array to string conversion` error

--- src/Component/Template.php
+++ src/Component/Template.php
@@ -116,6 +116,13 @@ class Template
                 throw new Exception("Unable to find template: {$selectedTemplate}");
             }
 
+            if (false !== strpos($e->getMessage(), 'to string conversion')) {
+                throw new Exception("Error: Array to string conversion. \n
+                    Try not to build complex data structures inside of Twig templates
+                     \n >> see: https://stackoverflow.com/questions/32633853/twig-array-to-string-conversion"
+                );
+            }
+
             throw $e;
         }
     }
